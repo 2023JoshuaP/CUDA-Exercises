e7341e8ab370ead413f357f452a8071eb25d609f
2023JoshuaP <jphilco@unsa.edu.pe>
2025-12-13 12:50:08 -0500

    Corrections on the syntax.

--- matrix_add.cu
+++ matrix_add.cu
@@ -1,4 +1,4 @@
-// %%writefile matrix_add.cu
+%%writefile matrix_add.cu
 #include <stdio.h>
 #include <cuda_runtime.h>
 #include <math.h>
@@ -28,18 +28,18 @@ void matrix_addition(float* c, float* a, float* b, int n, char solution) {
     if (solution == 'B') {
         dim_block = dim3(16, 16, 1);
         dim_grid = dim3((n + dim_block.x - 1) / dim_block.x, (n + dim_block.y - 1) / dim_block.y, 1);
-        printf("Exercise 1B: Grid(%d, %d)\n", dim_grid.x, dim_grid.y);
+        printf("Exercise 1.B: Grid(%d, %d)\n", dim_grid.x, dim_grid.y);
         matrix_add_element<<<dim_grid, dim_block>>>(d_c, d_a, d_b, n);
     }
     else if (solution == 'C') {
         dim_block = dim3(256, 1, 1);
-        dim_grid = ((n + dim_block.x - 1) / dim_block.x, 1, 1);
+        dim_grid = dim3((n + dim_block.x - 1) / dim_block.x, 1, 1);
         printf("Exercise 1.C: Grid(%d)\n", dim_grid.x);
         matrix_add_row<<<dim_grid, dim_block>>>(d_c, d_a, d_b, n);
     }
     else if (solution == 'D') {
         dim_block = dim3(256, 1, 1);
-        dim_grid = ((n + dim_block.x - 1) / dim_block.x, 1, 1);
+        dim_grid = dim3((n + dim_block.x - 1) / dim_block.x, 1, 1);
         printf("Exercise 1.D: Grid(%d)\n", dim_grid.x);
         matrix_add_column<<<dim_grid, dim_block>>>(d_c, d_a, d_b, n);
     }
@@ -55,9 +55,9 @@ void matrix_addition(float* c, float* a, float* b, int n, char solution) {
 
 __global__ void matrix_add_element(float* d_c, float* d_a, float* d_b, int n) {
     int columns = blockIdx.x * blockDim.x + threadIdx.x;
-    int rows = blñockIdx.y * blockDim.y + threadIdx.y;
+    int rows = blockIdx.y * blockDim.y + threadIdx.y;
 
-    if (cols < n && rows < n) {
+    if (columns < n && rows < n) {
         int index = rows * n + columns;
         d_c[index] = d_a[index] + d_b[index];
     }
@@ -65,7 +65,7 @@ __global__ void matrix_add_element(float* d_c, float* d_a, float* d_b, int n) {
 
 /* Exercise 1.C */
 
-__global__ matrix_add_row(float* d_c, float* d_a, float* d_b, int n) {
+__global__ void matrix_add_row(float* d_c, float* d_a, float* d_b, int n) {
     int row = blockIdx.y * blockDim.y + threadIdx.y;
 
     if (row < n) {
@@ -78,12 +78,12 @@ __global__ matrix_add_row(float* d_c, float* d_a, float* d_b, int n) {
 
 /* Exercise 1.D */
 
-__global__ matrix_add_column(float* d_c, float* d_a, float* d_b, int n) {
-    int column = blockIdx.x + blockDim.x + threadIdx.x;
+__global__ void matrix_add_column(float* d_c, float* d_a, float* d_b, int n) {
+    int column = blockIdx.x * blockDim.x * threadIdx.x;
 
     if (column < n) {
         for (int i = 0; i < n; i++) {
-            int index = col * n + i;
+            int index = i * n + column;
             d_c[index] = d_a[index] + d_b[index];
         }
     }
@@ -94,9 +94,9 @@ int main() {
     float* b = (float*)malloc(N * N * sizeof(float));
     float* c = (float*)malloc(N * N * sizeof(float));
 
-    for (int i = 0 i < N * N; i++) {
-        a[1] = 1.0f;
-        b[1] = 2.0f;
+    for (int i = 0; i < N * N; i++) {
+        a[i] = 1.0f;
+        b[i] = 2.0f;
     }
 
     matrix_addition(c, a, b, N, 'B');
@@ -108,7 +108,7 @@ int main() {
     }
     
     matrix_addition(c, a, b, N, 'C');
-    if (c[0] == N) {
+    if (c[0] == 3.0f) {
         printf("Exercise 1.C: PASSED\n");
     }
     for (int i = 0; i < N * N; i++) {
@@ -116,7 +116,7 @@ int main() {
     }
     
     matrix_addition(c, a, b, N, 'D');
-    if (c[0] == N) {
+    if (c[0] == 3.0f) {
         printf("Exercise 1.D: PASSED\n");
     }
     
